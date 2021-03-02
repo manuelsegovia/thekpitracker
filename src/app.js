197f28c39021305951b97efcd49b4009e6a95462
Manuel Segovia <msegovia@mac.com>
2021-03-02 17:25:33 -0500

    changed host

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const Hapi = require('@hapi/hapi');
 const plugins = require('./plugins');
 
+const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost';
 const createSever = async () => {
   const server = Hapi.server({
-    port: process.env.PORT || 8080,
-    host: process.env.HOST || 'localhost',
+    port: process.env.PORT,
+    host,
+    routes: {
+      cors: true,
+    },
   });
   await plugins.register(server);
   server.route(require('./routes'));
